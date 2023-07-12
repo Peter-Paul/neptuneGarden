3c4780a396287000fdfc42497281656d212263e6
Peter-Paul <ppmunga@gmail.com>
2023-07-12 03:21:22 +0300

    NFT platform

--- frontend/src/shared/about.js
+++ frontend/src/shared/about.js
@@ -3,8 +3,8 @@ function About() {
         <>
             <div className="text-center">
                 <h3>About Neptune Garden</h3>
-                <p className="mt-4">HAPE Mfers is an NFT collection inspired by the HAPE and mfers collections. The time when HAPE x mfers joins together.</p>
-                <p className="mt-4">We are HAPE Mfers. We grow together, building a strong community with a definite future.</p>
+                <p className="mt-4">Neptune Garden is an NFT collection inspired by the Neptune Garden collections.</p>
+                <p className="mt-4">We are Neptune Garden. We grow together, building a strong community with a definite future.</p>
                 <p>All unminted pieces have been locked inside the contract. Discord will be opened when the right time comes.</p>
             </div>
         </>
