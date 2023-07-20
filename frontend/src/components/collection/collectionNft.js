1376f0e680dec272af17a9ecdb232328150921bb
Peter-Paul <ppmunga@gmail.com>
2023-07-20 10:00:09 +0300

    Collection update

--- frontend/src/components/collection/collectionNft.js
+++ frontend/src/components/collection/collectionNft.js
@@ -2,17 +2,17 @@ function CollectionNft({nft}) {
     return ( 
 
         <>
-            <div className="d-flex flex-column">
-                <div className="justify-content-center">
+            <div className="d-flex justify-content-center">
+                <div className="d-flex flex-column">
 
                     <div  className="newNft-holder">
                         <div style={{backgroundImage:`url(${nft.Image})`}} 
                         className="newNft-img" alt=""></div>
                     </div>
-                </div>
-                <div className="d-flex justify-content-center">
-                    <div className="d-grid gap-2 col-3 mx-auto">
-                        <button className="btn btn-lg btn-outline-dark mt-2">Checkout</button>
+                    <div className="d-flex justify-content-center">
+                        <div className="d-grid gap-2 col-10">
+                            <button className="btn btn-outline-dark mt-2">VIEW IN MAGIC DEN</button>
+                        </div>
                     </div>
                 </div>
             </div>
